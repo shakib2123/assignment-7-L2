87e9b95f432cbaf80d747128dce6616fe58c6c69
shakib2123 <mshakibalhasan21@gmail.com>
2024-10-22 21:49:33 +0600

    query 4 completed

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -76,3 +76,7 @@ UPDATE students
 SET status = 'Awarded'
 WHERE (frontend_mark + backend_mark) = (SELECT MAX(frontend_mark + backend_mark) FROM students);
 
+-- Query 4: Delete all courses that have no students enrolled.
+DELETE FROM courses
+WHERE course_id NOT IN (SELECT course_id FROM enrollment);
+
