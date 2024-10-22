3edf78ee7aa811726d39cbfa311f3e03021f74a1
shakib2123 <mshakibalhasan21@gmail.com>
2024-10-22 20:23:36 +0600

    data inserted to students table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -8,3 +8,15 @@ CREATE TABLE students(
     backend_mark INTEGER,
     status VARCHAR(10)
 )
+
+
+INSERT INTO students(student_name,age,email,frontend_mark,backend_mark,status) 
+VALUES
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
