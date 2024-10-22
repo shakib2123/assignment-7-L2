882e12df71f1d245ed900def3ccba3062d34d300
shakib2123 <mshakibalhasan21@gmail.com>
2024-10-22 20:33:48 +0600

    courses table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,7 @@
 -- Active: 1729271827172@@127.0.0.1@5432@university_db
+
+
+-- Create a "students" table
 CREATE TABLE students(
     student_id SERIAL PRIMARY KEY,
     student_name VARCHAR(50),
@@ -9,7 +12,7 @@ CREATE TABLE students(
     status VARCHAR(10)
 )
 
-
+-- Insert data into the "students" table
 INSERT INTO students(student_name,age,email,frontend_mark,backend_mark,status) 
 VALUES
 ('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
@@ -18,5 +21,14 @@ VALUES
 ('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
 ('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
 ('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
+
+-- Create a "courses" table
+CREATE TABLE courses(
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50),
+    credits INTEGER
+);
 
 
